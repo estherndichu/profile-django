f9b7f8822e39ad86dd8b184e3af4d8ddd52bf36b
estherndichu <itskuijenga@gmail.com>
2021-04-02 10:16:26 +0300

    registration and sign up form

--- instar/models.py
+++ instar/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     photo = CloudinaryField('profile')
     bio = models.CharField(max_length=200)
+    followers = models.ManyToManyField(User, related_name="followers", blank=True)
 
     def __str__(self):
         return self.user.username
@@ -35,3 +36,8 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.name} - {self.caption}"
 
+class Comment(models.Model):
+    comment = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.comment

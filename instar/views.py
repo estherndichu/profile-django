8242644f6e581e0273095e094f9a71d8930b7c90
estherndichu <itskuijenga@gmail.com>
2021-04-03 06:10:25 +0300

    add comments to models

--- instar/views.py
+++ instar/views.py
@@ -7,32 +7,33 @@ from django.contrib.auth import logout, login, authenticate
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-@login_required(login_url='/accounts/login/')
+# @login_required(login_url='/accounts/login/')
 def index(request):
-    current_user = request.user
-    print(current_user)
-    current_profile = Profile.objects.get(user_id=current_user)
-    posts = Post.objects.all()[::-1]
+    # current_user = request.user
+    # print(current_user)
+    # current_profile = Profile.objects.get(user_id=current_user)
+    # posts = Post.objects.all()[::-1]
 
-    if request.method == "POST":
-        post_form = PostForm(request.POST, request.FILES)
+    # if request.method == "POST":
+    #     post_form = PostForm(request.POST, request.FILES)
 
-        if post_form.is_valid():
-            post = post_form.save(commit=False)
+    #     if post_form.is_valid():
+    #         post = post_form.save(commit=False)
 
-            post.profile = current_user
-            post.user_profile = current_profile
+    #         post.profile = current_user
+    #         post.user_profile = current_profile
 
-            post.save()
-            post_form = PostForm()
-            return HttpResponseRedirect(reverse("index"))
+    #         post.save()
+    #         post_form = PostForm()
+    #         return HttpResponseRedirect(reverse("index"))
 
-    else:
-        post_form = PostForm()
+    # else:
+    #     post_form = PostForm()
 
     
 
-    return render(request, "instar/index.html",{"posts":posts,"current_user":current_user,"current_profile":current_profile,"post_form":post_form})
+    # return render(request, "instar/index.html",{"posts":posts,"current_user":current_user,"current_profile":current_profile,"post_form":post_form})
+    return render(request,'instar/index.html')
 
 
 def post(request,id):

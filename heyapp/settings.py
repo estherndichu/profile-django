6e245dfeca2399b914b1e6988758dbd9a5e42c51
estherndichu <itskuijenga@gmail.com>
2021-04-06 12:15:59 +0300

    connect to heroku postgres

--- heyapp/settings.py
+++ heyapp/settings.py
@@ -95,14 +95,18 @@ WSGI_APPLICATION = 'heyapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'instar',
-        'USER': 'esther',
-    'PASSWORD':'brielle',
-    }
-}
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': os.environ.get('DB_NAME'),
+           'USER': os.environ.get('DB_USER'),
+           'PASSWORD': os.environ.get('DB_PASSWORD'),
+           'HOST': os.environ.get('DB_HOST'),
+           'PORT': '5432',
+       }
+       
+   }
 
 
 # Password validation
